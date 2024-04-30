2dc0c77657c6132a30cf518f1be0f6ec92b679b6
Marekus <marek.stransky1@gmail.com>
2024-04-30 15:20:12 +0200

    Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../assets/logo/logo.svg';
+
+const Header = () => {
+    return (
+        <header className='h-20 w-full border-b border-gray-400 fixed px-10'>
+            <div className='flex items-center h-full gap-x-2'>
+                <img src={Logo} alt="Logo" className='w-12 h-12' />
+                <h1 className='font-pt_sans text-secondary text-xl'>GreenHouse</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
