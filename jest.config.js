c1af40ba8cca0f03e85547fa1ec0a463e5df186d
Marekus <marek.stransky1@gmail.com>
2024-05-14 22:38:44 +0200

    Test: General

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
-// jest.config.js
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
-    },
-    moduleNameMapper: {
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-    testEnvironment: 'jsdom'
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: '@happy-dom/jest-environment',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios|primereact)/.*)',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(svg|jpg|jpeg|png|gif|webp|avif)$': '<rootDir>/src/__mocks__/fileMock.js',
+    'primereact/resources/themes/saga-blue/theme.css': '<rootDir>/src/__mocks__/primereact.css.js',
+    'primereact/resources/primereact.min.css': '<rootDir>/src/__mocks__/primereact.css.js',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
--- src/pages/Login/__tests__/Login.test.tsx
+++ src/pages/Login/__tests__/Login.test.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
 import Login from '../Login';
 
 describe('Login Component', () => {
   test('renders login form', () => {
-    render(<Login />);
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
     expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
   });
 
   test('submits login form', () => {
-    render(<Login />);
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
     const usernameInput = screen.getByPlaceholderText('Username');
     const passwordInput = screen.getByPlaceholderText('Password');
     const loginButton = screen.getByRole('button', { name: 'Log in' });
