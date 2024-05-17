44419879bd842ecfd8eac4d70dd84ea0a063c596
Marekus <marek.stransky1@gmail.com>
2024-05-17 16:03:28 +0200

    SEP-129: Body

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,9 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = () => {
     return (
-        <div>Home</div>
+        <div className='pt-[100px] px-5 lg:pl-[272px] xl:pl-[344px] lg:pr-4 xl:pr-6'>
+            Home
+        </div>
     );
 };
 
