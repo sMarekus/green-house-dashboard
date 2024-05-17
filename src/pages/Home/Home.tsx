5624c40c739a2ca9467d03d3cbe48b166b75ee88
Marekus <marek.stransky1@gmail.com>
2024-05-17 16:10:47 +0200

    SEP-129: Style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,21 @@ interface HomeProps {
 const Home: React.FC<HomeProps> = () => {
     return (
         <div className='pt-[100px] px-5 lg:pl-[272px] xl:pl-[344px] lg:pr-4 xl:pr-6'>
-            Home
+            <h1 className='font-pt_sans_arrow text-secondary 2xl:text-5xl'>Welcome Back!</h1>
+
+            <div className='flex justify-between 2xl:gap-x-10 2xl:pt-8'>
+                <div className='w-1/3 h-40 bg-red-500'>
+
+                </div>
+
+                <div className='w-1/3 h-40 bg-red-500'>
+                    
+                </div>
+
+                <div className='w-1/3 h-40 bg-red-500'>
+                    
+                </div>
+            </div>
         </div>
     );
 };
