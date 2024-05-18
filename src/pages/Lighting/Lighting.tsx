653e3cacba0102eb38d2fd2b7d6f50c8d4095da2
apurveh <apurvamishra573@gmail.com>
2024-05-18 11:40:56 +0200

    SEP-126: Structure

--- src/pages/Lighting/Lighting.tsx
+++ src/pages/Lighting/Lighting.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import ThresholdButton from '../../components/ThresholdButton'; // Update the path if needed
+import LightBulbIcon from '../../components/Icons/LightBulb';
+import Button from 'primereact/button';
+import './Lighting.sass';
 
-interface LightingProps {
-    
-}
-
-const Lighting: React.FC<LightingProps> = () => {
+const Lighting: React.FC = () => {
     return (
-        <div>Lighting</div>
+        <div className="lighting-page">
+            <h1 className='font-pt_sans_arrow text-secondary text-3xl md:text-3xl xl:text-4xl 2xl:text-5xl mb-8'>Lighting</h1>
+            <div className="mb-5">
+                <LightBulbIcon className="lightbulb-icon" />
+            </div>
+            <div className="flex items-center mb-24 font-pt_sans_arrow text-secondary text-3xl md:text-3xl xl:text-4xl 2xl:text-5xl">
+                <button className="bg-none">+</button>
+                <p className='mx-6 my-6'>584568</p>
+                <button className="bg-none">-</button>
+            </div>
+            <div className="w-80">
+                <ThresholdButton label="Set Notification Threshold" />
+            </div>
+        </div>
     );
 };
 
