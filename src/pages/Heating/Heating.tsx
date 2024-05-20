9b8deb89895f36ddceefd8ee90eef3c039506645
apurveh <apurvamishra573@gmail.com>
2024-05-20 23:27:30 +0200

    SEP-126, 127, 128 : Made the threshold a reusable component and used it for lighting, heating and humidity.

--- src/pages/Heating/Heating.tsx
+++ src/pages/Heating/Heating.tsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react';
 import HeatingIcon from '../../components/Icons/Sun';
-import { Button } from 'primereact/button';
 import ThresholdButton from '../../components/ThresholdButton';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
+import ThresholdModal from '../../components/ThresholdModal';
 
 interface HeatingProps {}
 
 const Heating: React.FC<HeatingProps> = () => {
     const [visible, setVisible] = useState(false);
-    const FooterContent = (
-        <div className="flex justify-end space-x-2">
-            <Button label="Set Notification Threshold" className="bg-primary font-pt_sans" />
-            <Button label="Cancel" className="p-button-warning font-pt_sans" onClick={() => setVisible(false)} autoFocus />
-        </div>
-    );
+
+    const handleConfirm = () => {
+        // Logic for setting up threshold
+        setVisible(false);
+    };
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
@@ -27,19 +24,12 @@ const Heating: React.FC<HeatingProps> = () => {
             </div>
             <div className="w-full md:w-80 px-4 md:px-0">
                 <ThresholdButton label="Set Notification Threshold" onClick={() => setVisible(true)} />
-                <Dialog
+                <ThresholdModal
                     visible={visible}
-                    footer={FooterContent}
                     onHide={() => setVisible(false)}
-                    className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3"
-                >
-                    <div className="p-4">
-                        <InputText
-                            className="w-full px-4 focus:shadow-none font-pt_sans"
-                            placeholder="Set Maximum Heating Level"
-                        />
-                    </div>
-                </Dialog>
+                    placeholder="Set Maximum Heating Level"
+                    onConfirm={handleConfirm}
+                />
             </div>
         </div>
     );
--- src/pages/Lighting/Lighting.tsx
+++ src/pages/Lighting/Lighting.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ThresholdButton from '../../components/ThresholdButton';
 import LightBulbIcon from '../../components/Icons/LightBulb';
+import ThresholdModal from '../../components/ThresholdModal';
 
 const Lighting: React.FC = () => {
+    const [visible, setVisible] = useState(false);
+
+    const handleConfirm = () => {
+        // Logic for setting up threshold
+        setVisible(false);
+    };
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className='font-pt_sans_arrow text-secondary text-3xl md:text-3xl xl:text-4xl 2xl:text-5xl mb-8'>Lighting</h1>
@@ -15,7 +22,13 @@ const Lighting: React.FC = () => {
                 <button className="bg-none">-</button>
             </div>
             <div className="w-full md:w-80 px-4 md:px-0">
-                <ThresholdButton label="Set Notification Threshold" />
+                <ThresholdButton label="Set Notification Threshold" onClick={() => setVisible(true)} />
+                <ThresholdModal
+                    visible={visible}
+                    onHide={() => setVisible(false)}
+                    placeholder="Set Maximum Lighting Level"
+                    onConfirm={handleConfirm}
+                />
             </div>
         </div>
     );
