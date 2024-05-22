552254b61c9714cb0766308b1aafd602b54c29be
Marekus <marek.stransky1@gmail.com>
2024-05-22 15:20:49 +0200

    API & Service: Board

---
+++ src/api/board.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_URL = 'http://sep4backendapp.azurewebsites.net/api/Board';
+
+export const getStatuses = async () => {
+    try {
+      const windowResponse = await axios.get(`${API_URL}/window-status`);
+      const ledResponse = await axios.get(`${API_URL}/led-status`);
+      return { windowStatus: windowResponse.data, ledStatus: ledResponse.data };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new Error('Failed to fetch statuses: ' + error.message);
+      } else {
+        throw new Error('Failed to fetch statuses');
+      }
+    }
+};
+  
+  export const updateWindowStatus = async (windowStatus: number, ledStatus: number) => {
+    try {
+      await axios.patch(`${API_URL}/update-status`, { windowStatus, ledStatus });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new Error('Failed to update window status: ' + error.message);
+      } else {
+        throw new Error('Failed to update window status');
+      }
+    }
+};
