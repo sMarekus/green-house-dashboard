c4851c4e96b57dfeac7a5cdc236e90668dbf857d
apurveh <apurvamishra573@gmail.com>
2024-05-22 17:24:15 +0200

    SEP-153 : LED Functionality Completed

--- src/api/Services/BoardService.ts
+++ src/api/Services/BoardService.ts
@@ -1,11 +1,11 @@
-import { getStatuses, updateWindowStatus } from '../../api/board';
+import { getStatuses, updateStatus } from '../../api/board';
 
 export const BoardService = {
   getStatuses: async () => {
     return await getStatuses();
   },
   
-  updateWindowStatus: async (windowStatus: number, ledStatus: number) => {
-    return await updateWindowStatus(windowStatus, ledStatus);
+  updateStatus: async (windowStatus: number, ledStatus: number) => {
+    return await updateStatus(windowStatus, ledStatus);
   },
 };
