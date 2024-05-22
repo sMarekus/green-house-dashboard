dd6a0f4bd4dd7f8b23d5d4298885f7e66f3450ec
apurveh <apurvamishra573@gmail.com>
2024-05-22 15:32:42 +0200

    SEP 134 : Latest Humidity Measurement and setting humidity threshold completed

--- src/pages/Humidity/Humidity.tsx
+++ src/pages/Humidity/Humidity.tsx
@@ -1,7 +1,9 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect } from 'react';
 import HumidityIcon from '../../components/Icons/Humidity';
 import ThresholdButton from '../../components/ThresholdButton';
 import ThresholdModal from '../../components/ThresholdModal';
+import { MeasurementService } from '../../api/Services/MeasurementService';
+import { NotificationService } from '../../api/Services/NotificationService';
 
 interface HumidityProps {
     
@@ -9,11 +11,35 @@ interface HumidityProps {
 
 const Humidity: React.FC<HumidityProps> = () => {
     const [visible, setVisible] = useState(false);
+    const [humidity, setHumidity] = useState<number | null>(null);
+    
+    useEffect(() => {
+        const fetchHumidity = async () => {
+            try {
+                const latestHumidity = await MeasurementService.getLatestHumidityMeasurement();
+                setHumidity(latestHumidity);
+            } catch (error) {
+                console.error('Failed to fetch latest humidity:', error);
+            }
+        };
 
-    const handleConfirm = () => {
-        // Logic for setting up threshold
-        setVisible(false);
+        fetchHumidity(); // Initial fetch
+
+        const intervalId = setInterval(fetchHumidity, 14400000); // Fetch every 4hrs
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const handleConfirm = async (threshold: number) => {
+        try {
+            await NotificationService.setHumidityNotification(threshold);
+            console.log("Successfully set humidity notification threshold.");
+            setVisible(false);
+        } catch (error) {
+            console.error('Failed to set humidity notification:', error);
+        }
     };
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
         <h1 className='font-pt_sans_arrow text-secondary text-3xl md:text-3xl xl:text-4xl 2xl:text-5xl mb-12'>Humidity</h1>
@@ -21,7 +47,7 @@ const Humidity: React.FC<HumidityProps> = () => {
             <HumidityIcon className="w-40 h-40" color='#BBBB' />
         </div>
         <div className="flex justify-center mb-28 font-pt_sans_arrow text-secondary text-3xl md:text-3xl xl:text-4xl 2xl:text-5xl">
-            <p>40%</p>
+        <p>{humidity !== null ? `${humidity} %` : 'Loading...'}</p>
         </div>
         <div className="w-full md:w-80 px-4 md:px-0">
             <ThresholdButton label="Set Notification Threshold" onClick={() => setVisible(true)}/>
